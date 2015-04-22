a3794765e3c2328f7adec1c2f35c9ffb6d631549
decmoran <0145076@studentmail.ul.ie>
2015-04-22 16:44:11 +0100

    Some What Working
    Will now load to phone after changing eclipse.ini settings. Force closes
    on start. Issue on line 12 of some unspecified .xml file. Assuming
    activity_main.xml (fragment)

--- MapTest/src/ie/ul/id0145076/maptest/MainActivity.java
+++ MapTest/src/ie/ul/id0145076/maptest/MainActivity.java
@@ -2,9 +2,10 @@ package ie.ul.id0145076.maptest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
